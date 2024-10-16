9ae7634acdf0c829b7b6c5c551ba7746a6a4b395
AdamBrown04 <adambrown2004@outlook.com>
2024-10-16 17:17:07 +0100

    added attributes to Car class, renamed User to Car

---
+++ CarRentalSystem/Car.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarRentalSystem
+{
+    internal class Car
+    {
+        private string make;
+        private string model;
+        private string yearOfManufacture;
+        private string regPlate;
+        private float costToRent;
+        private string bodyType;
+    }
+}
