615f50299c983822507a9f677a91295cb1232539
AdamBrown04 <adambrown2004@outlook.com>
2024-10-30 14:49:04 +0000

    added view operations to User

--- CarRentalSystem/User.cs
+++ CarRentalSystem/User.cs
@@ -10,11 +10,47 @@ namespace CarRentalSystem
     {
         private string fName;
         private string lName;
-        private string userName;
+        private string username;
         private string password;
         private string email;
         private string DoB;
         private bool isStaff;
         private List<string> rentHistory = new List<string>();
+
+        public void ViewfName()
+        {
+            Console.Write(fName);
+        }
+        public void ViewlName()
+        {
+            Console.Write(lName);
+        }
+        public void ViewUsername()
+        {
+            Console.Write(username);
+        }
+        public void ViewPassword()
+        {
+            Console.Write(password);
+        }
+        public void ViewEmail()
+        {
+            Console.Write(email);
+        }
+        public void ViewDoB()
+        {
+            Console.Write(DoB);
+        }
+        public void ViewIsStaff()
+        {
+            if (isStaff)
+            {
+                Console.Write($"{username} is staff");
+            }
+            else
+            {
+                Console.Write($"{username} is not staff");
+            }
+        }
     }
 }
