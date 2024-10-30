ebd25ed374cde8f0b0082b98d203f91a4725251e
AdamBrown04 <adambrown2004@outlook.com>
2024-10-30 13:21:02 +0000

    commented out the class creation and file reading for now, added a log in system

--- CarRentalSystem/Program.cs
+++ CarRentalSystem/Program.cs
@@ -1,13 +1,28 @@
 ﻿using CarRentalSystem;
-
+/*
 string uPath = "Users.txt";
 string cPath = "Cars.txt";
 
 List<Car> cars = new List<Car>();
 List<User> users = new List<User>();
+*/
 
-Console.Write("Username: ");
-string uName = Console.ReadLine();
+while (true)
+{
+    Console.Write("Username: ");
+    string uName = Console.ReadLine();
 
-Console.Write("Password: ");
-string password = Console.ReadLine();
+    Console.Write("Password: ");
+    string password = Console.ReadLine();
+
+    if (uName.ToLower() == "admin" && password.ToLower() == "password")//placeholder for now
+    {
+        break;
+    }
+    else
+    {
+        Console.WriteLine("USER DOES NOT EXIST/PASSWORD IS INCORRECT");
+        Task.Delay(1500).Wait();
+        Console.Clear();
+    }
+}
