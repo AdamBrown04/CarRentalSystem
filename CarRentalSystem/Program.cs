d93adb7e6c2301a0a9b49a6919f5a722a5d62c2c
AdamBrown04 <adambrown2004@outlook.com>
2024-12-04 12:05:32 +0000

    added comment

--- CarRentalSystem/Program.cs
+++ CarRentalSystem/Program.cs
@@ -70,7 +70,7 @@ if (currentUser.GetIsStaff())
     while (!exit)
     {
         Console.Clear();
-        Console.WriteLine("MENU \n1)Add a new vehicle \n2)Remove a vehicle \n3)Add a new user \n4)Remove a user \n5) \n6)Available vehicles \n7)Close program");
+        Console.WriteLine("MENU \n1)Add a new vehicle \n2)Remove a vehicle \n3)Add a new user \n4)Remove a user \n5)Remove a person who's currently renting \n6)Available vehicles \n7)Close program");
         string userOption = Console.ReadLine().Trim();
         Console.Clear();
         switch (userOption) 
@@ -221,7 +221,7 @@ if (currentUser.GetIsStaff())
                 }
                 break;
             case "5":
-                
+                //use parallel.foreach to display all vehicles that are currently rented with email of renter, get user to input email, confirm, remove current renter from instance
                 break;
             case "6":
                 GetAvailableVehicles();
